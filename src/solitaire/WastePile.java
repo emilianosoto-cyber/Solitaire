c8225f895cc70a1d0cf5633446f4aafa953a160a
Emiliano Soto <emiliano.soto@uabc.edu.mx>
2026-03-11 23:38:12 -0700

    Reescribimos la clase waste para implementar la clase pila en ellas y dejar de usar ArrayList.

--- src/solitaire/WastePile.java
+++ src/solitaire/WastePile.java
@@ -3,6 +3,7 @@ package solitaire;
 import DeckOfCards.CartaInglesa;
 
 import java.util.ArrayList;
+
 /**
  * Modela el montículo donde se colocan las cartas
  * que se extraen de Draw pile.
@@ -11,21 +12,30 @@ import java.util.ArrayList;
  * @version (2025-2)
  */
 public class WastePile {
-    private ArrayList<CartaInglesa> cartas;
 
-    public WastePile() {
-        cartas = new ArrayList<>();
-    }
+    //Implementamos pilas (clase)
+    private Pila<CartaInglesa> cartas = new Pila<>();
+
+    //public WastePile() {
+        //la pila ya se inicializo, ya no es necesario
+
 
     public void addCartas(ArrayList<CartaInglesa> nuevas) {
-        cartas.addAll(nuevas);
+        //Como la Pila no tiene addAll, agregamos carta por carta con el push()
+        for (CartaInglesa carta : nuevas){
+            cartas.push(carta);
+        }
     }
 
     public ArrayList<CartaInglesa> emptyPile() {
         ArrayList<CartaInglesa> pile = new ArrayList<>();
-        if (!cartas.isEmpty()) {
-            pile.addAll(cartas);
-            cartas = new ArrayList<>();
+        if (!cartas.isEmpty()){
+
+            //sacamos carta por carta con pop() hasta vaciar la pila
+            while (!cartas.isEmpty()){
+                pile.add(cartas.pop());
+            }
+            //La pila ya quedó vacía después del while, no necesitamos limpiarla
         }
         return pile;
     }
@@ -34,19 +44,14 @@ public class WastePile {
      * Obtener la última carta sin removerla.
      * @return Carta que está encima. Si está vacía, es null.
      */
-    public CartaInglesa verCarta() {
-        CartaInglesa regresar = null;
-        if (!cartas.isEmpty()) {
-            regresar = cartas.getLast();
-        }
-        return regresar;
+    public CartaInglesa verCarta(){
+        //el peek ve el tope sin quitarlo y regresa null si está vacía
+        return cartas.peek();
     }
+
     public CartaInglesa getCarta() {
-        CartaInglesa regresar = null;
-        if (!cartas.isEmpty()) {
-            regresar = cartas.removeLast();
-        }
-        return regresar;
+        //pop quita y regresa el tope, regresa null si está vacía (nuestra clase Pila regresa null en pop() cuando está vacía)
+        return cartas.pop();
     }
 
     @Override
@@ -55,7 +60,8 @@ public class WastePile {
         if (cartas.isEmpty()) {
             stb.append("---");
         } else {
-            CartaInglesa regresar = cartas.getLast();
+            //peek ve el tope sin quitarlo
+            CartaInglesa regresar = cartas.peek();
             regresar.makeFaceUp();
             stb.append(regresar.toString());
         }
