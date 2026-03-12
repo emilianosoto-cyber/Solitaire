6f6eab274d9753d8c62ca8db9c63b2ea5541ac22
Emiliano Soto <emiliano.soto@uabc.edu.mx>
2026-03-12 01:04:46 -0700

    Se agregan métodon para guardar y undo en la clase FoundationDeck. Aparte se agrega un método para restaurar las foundation.

--- src/solitaire/FoundationDeck.java
+++ src/solitaire/FoundationDeck.java
@@ -2,6 +2,8 @@ package solitaire;
 
 import DeckOfCards.CartaInglesa;
 import DeckOfCards.Palo;
+import java.util.ArrayList;
+
 
 public class FoundationDeck {
 
@@ -76,4 +78,45 @@ public class FoundationDeck {
     public Palo getPalo() {
         return palo;
     }
+
+
+    //Método que regresa todas las cartas en una Arraylist
+    //guardamos el estado del juego antes de un movimiento
+    public ArrayList<CartaInglesa> getCartasComoLista(){
+
+        ArrayList<CartaInglesa> lista = new ArrayList<>();
+        //pila para no perder el orden original (pila temp.)
+        Pila<CartaInglesa> temp = new Pila<>();
+
+        //copiamos la pila original en una temporal
+        while (!cartas.isEmpty()){
+
+            temp.push(cartas.pop());
+        }
+
+        //devolvemos las cartas a la pila original y al mismo tiempo las agregamos a la lista
+        while (!temp.isEmpty()){
+
+            CartaInglesa carta = temp.pop();
+            cartas.push(carta);
+            lista.add(carta);
+        }
+
+        return lista;
+    }
+
+    //Regresa foundation desde una lista anteriormente guardada
+    public void restaurar(ArrayList<CartaInglesa> lista){
+        //Vaciamos la pila actual
+        cartas.clear();
+
+        // Agregamos carta por carta desde la lista guardada
+        for (CartaInglesa carta : lista){
+
+            cartas.push(carta);
+        }
+
+    }
+
+
 }
